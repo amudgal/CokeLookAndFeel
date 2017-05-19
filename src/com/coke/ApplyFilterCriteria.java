518f1cd533db6257b80c943f0a6a5519de3f80e3
Amit Mudgal <amit.k.mudgal@gmail.com>
2017-05-18 19:48:06 -0700

    Added comment section

--- src/com/coke/ApplyFilterCriteria.java
+++ src/com/coke/ApplyFilterCriteria.java
@@ -1,5 +1,7 @@
 /**
- * MicroStrategy SDK Sample
+ * Created by Amit Mudgal (Senior Principal Consultant) 
+ * Date : 5/18/2017
+ * 
  *
  * Copyright © 2017 MicroStrategy Incorporated. All Rights Reserved.
  *
@@ -18,7 +20,13 @@ package com.coke;
 
 import com.microstrategy.web.app.addons.AbstractAppAddOn;
 import com.microstrategy.web.app.beans.PageComponent;
+import com.microstrategy.web.beans.PromptsBean;
 import com.microstrategy.web.beans.RWBean;
+import com.microstrategy.web.beans.WebBeanException;
+import com.microstrategy.web.objects.WebDocumentInstance;
+import com.microstrategy.web.objects.WebObjectsException;
+import com.microstrategy.web.objects.WebPrompts;
+import com.microstrategy.web.objects.rw.RWInstance;
 
 
 public class ApplyFilterCriteria extends AbstractAppAddOn { 
@@ -30,7 +38,22 @@ public class ApplyFilterCriteria extends AbstractAppAddOn {
     public void preCollectData(PageComponent page) {
     	System.out.println("Inside PreCollect");
     	RWBean rwb = (RWBean) page.getChildByClass(RWBean.class);
-
+    	
+    	try {
+    		RWInstance rwbi = rwb.getRWInstance();
+			if(rwbi.isPrompted()){
+			PromptsBean pbs = rwb.getPromptsBean();
+			WebPrompts prompts = pbs.getPrompts();
+			System.out.println("prompt size: " +prompts.size());
+			
+			}
+		} catch (WebObjectsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WebBeanException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	super.preCollectData(page);
     }
 
