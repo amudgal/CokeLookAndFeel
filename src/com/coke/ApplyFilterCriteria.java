2f7177cd6733014bf7b037d6fde899af90d18fca
Amit Mudgal <amit.k.mudgal@gmail.com>
2017-05-19 08:26:17 -0700

    Days work 5/18/2017

--- src/com/coke/ApplyFilterCriteria.java
+++ src/com/coke/ApplyFilterCriteria.java
@@ -18,6 +18,8 @@
 
 package com.coke;
 
+import java.util.ResourceBundle;
+
 import com.microstrategy.web.app.addons.AbstractAppAddOn;
 import com.microstrategy.web.app.beans.PageComponent;
 import com.microstrategy.web.beans.PromptsBean;
@@ -25,27 +27,39 @@ import com.microstrategy.web.beans.RWBean;
 import com.microstrategy.web.beans.WebBeanException;
 import com.microstrategy.web.objects.WebDocumentInstance;
 import com.microstrategy.web.objects.WebObjectsException;
+import com.microstrategy.web.objects.WebPrompt;
 import com.microstrategy.web.objects.WebPrompts;
 import com.microstrategy.web.objects.rw.RWInstance;
 
 
 public class ApplyFilterCriteria extends AbstractAppAddOn { 
- 
+
+    
     public String getAddOnDescription() {
         return "";
     }
-
+    private static final String PROPERTIES_FILE_NAME = "resources/configuration";
+    private static final String PROMPT_ID = "PROMPT_ID";
+    
     public void preCollectData(PageComponent page) {
+
     	System.out.println("Inside PreCollect");
     	RWBean rwb = (RWBean) page.getChildByClass(RWBean.class);
     	
     	try {
     		RWInstance rwbi = rwb.getRWInstance();
 			if(rwbi.isPrompted()){
-			PromptsBean pbs = rwb.getPromptsBean();
-			WebPrompts prompts = pbs.getPrompts();
-			System.out.println("prompt size: " +prompts.size());
-			
+			  PromptsBean pbs = rwb.getPromptsBean();
+			  WebPrompts prompts = pbs.getPrompts();
+			  //System.out.println("prompt size: " +prompts.size());
+			  ResourceBundle props = ResourceBundle.getBundle(PROPERTIES_FILE_NAME);
+			  String PromptGUID = (String) props.getObject(PROMPT_ID);
+			  for(int i=0;i<prompts.size();i++){
+				  WebPrompt prompt = prompts.get(i);
+                  if(PromptGUID.equalsIgnoreCase(prompt.getID())){
+                	  System.out.println("Positive Hit");
+                  }
+			  }
 			}
 		} catch (WebObjectsException e) {
 			// TODO Auto-generated catch block
