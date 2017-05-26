0aaf56e196576f25c358da3d3cda8329788f2b78
Amit Mudgal <amit.k.mudgal@gmail.com>
2017-05-26 13:21:39 -0700

    Protype for dynamic images

--- Controllers/PopUpWidgetController.js
+++ Controllers/PopUpWidgetController.js
@@ -14,5 +14,15 @@ PopApp.controller('PopAppController', function($scope) {
 
 $( document ).ready(function() {
     console.log( "ready!" );
+    var data = {
+		    "i_name":"<button class=\"w3-button w3-xlarge w3-circle w3-teal\">A</button>," +
+		             "<button class=\"w3-button w3-xlarge w3-circle w3-teal\">B</button>," +
+		             "<button class=\"w3-button w3-xlarge w3-circle w3-teal\">C</button>," +
+		             "<button class=\"w3-button w3-xlarge w3-circle w3-teal\">D</button>" 
+		};
+    var array = data.i_name.split(',');
+    for (a in array ) {
+    	$(array[a]).appendTo("div#level1");
+    }
     $("<button class=\"w3-button w3-xlarge w3-circle w3-black\">D</button>").appendTo("div#level1");
 });
