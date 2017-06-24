373e1282570b4be2cc8d10ad0854ab580083da10
Amit Mudgal <amit.k.mudgal@gmail.com>
2017-06-23 23:14:46 -0700

    Documents run indipendently

--- Controllers/MainController.js
+++ Controllers/MainController.js
@@ -69,66 +69,81 @@ var app = angular.module("RnavApp", ["ngRoute","angularModalService"]);
 	});
 	
 app.controller("HomeCntrl",function($scope){
-    $scope.docName ="Landing Page";	
+    $scope.docName ="Landing Page";
+    updatePath($scope);
  });
 
 app.controller("FLandCntrl",function($scope){
     $scope.docName ="Finance Landing Page";	
+    updatePath($scope);
  });
 
 app.controller("fMetricWFCntrl",function($scope){
-    $scope.docName ="Finance Metric Water Fall";	
+    $scope.docName ="Finance Metric Water Fall";
+    updatePath($scope);
  });
 
 app.controller("fPBTWFCntrl",function($scope){
-    $scope.docName ="Finance PBT Waterfall";	
+    $scope.docName ="Finance PBT Waterfall";
+    updatePath($scope);
  });
 
 app.controller("fScorecardCntrl",function($scope){
-    $scope.docName ="Finance Scorecard";	
+    $scope.docName ="Finance Scorecard";
+    updatePath($scope);
  });
 
 app.controller("fMetricDtlCntrl",function($scope){
-    $scope.docName ="Finance Metric Detail";	
+    $scope.docName ="Finance Metric Detail";
+    updatePath($scope);
  });
 
 app.controller("fPNLDtlCntrl",function($scope){
-    $scope.docName ="Finance P&L Detail";	
+    $scope.docName ="Finance P&L Detail";
+    updatePath($scope);
  });
 
 app.controller("fPNLCmpCntrl",function($scope){
-    $scope.docName ="Finance P&L Comparative";	
+    $scope.docName ="Finance P&L Comparative";
+    updatePath($scope);
  });
 
 app.controller("fTrendCntrl",function($scope){
-    $scope.docName ="Finance Trend";	
+    $scope.docName ="Finance Trend";
+    updatePath($scope);
  });
 
 app.controller("fQtrPlacementCntrl",function($scope){
-    $scope.docName ="Finance Quarterly Placement";	
+    $scope.docName ="Finance Quarterly Placement";
+    updatePath($scope);
  });
 
 app.controller("sTrxCntrl",function($scope){
-    $scope.docName ="Strategy Transactions";	
+    $scope.docName ="Strategy Transactions";
+    updatePath($scope);
  });
 
 app.controller("sShrDtlCntrl",function($scope){
-    $scope.docName ="Strategy Share Details";	
+    $scope.docName ="Strategy Share Details";
+    updatePath($scope);
  });
 
 app.controller("sShrBrndCntrl",function($scope){
-    $scope.docName ="Strategy Share Brand";	
+    $scope.docName ="Strategy Share Brand";
+    updatePath($scope);
  });
 
 app.controller("sIncidenceCntrl",function($scope){
     $scope.docName ="Strategy Incedence";
+    updatePath($scope);
  });
 
 app.controller("bBooksCntrl",function($scope){
     $scope.docName ="Brefing Books";
+    updatePath($scope);
  });
 app.controller("MainController",function($scope, ModalService){
-    $scope.pathsNew=" OPS Change";
+
     $scope.show = function() {
 	
         ModalService.showModal({
@@ -144,3 +159,65 @@ app.controller("MainController",function($scope, ModalService){
 })
 
 
+function getNameOfPath(element,srcJSON){
+	
+	return "nonne";
+}
+
+function getCookieValue(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+   // console.log(ca);
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+     //   	console.log(c);
+      //  	console.log(c.substring(name.length, c.length).replace('/"/g','').replace(/'/g,'"'));
+        	var retStr = c.substring(name.length, c.length).replace('/"/g','').replace(/'/g,'"');
+            return retStr.substring(0,retStr.length);
+        }
+    }
+    return "";
+}
+
+function updatePath($scope){
+	console.log("inside the update Path");
+	try{
+		$scope.srcJSON = jQuery.parseJSON( getCookieValJSON('Options').replace('/"/g','').replace(/'/g,'"'));
+	}catch(err){
+		console.log('Error' + err);
+	}
+	var element=getCookieValue('topLvl');
+	console.log(element);
+	var level="";
+	if(element==''){
+	    $scope.pathsNew=" CCNA";
+	}else{
+		level=element.replace(/\./g, '').substr(-3);
+		$scope.pathsNew=getNameOfPath(level,$scope.srcJSON);
+	}
+	console.log(level + ":::::" +$scope.pathsNew + ":::::" + element.length + "::::" + element);
+	
+}
+
+function getCookieValJSON(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+      //  	console.log(c);
+      //  	console.log(c.substring(name.length, c.length).replace('/"/g','').replace(/'/g,'"'));
+        	var retStr = c.substring(name.length, c.length).replace('/"/g','').replace(/'/g,'"');
+            return retStr.substring(1,retStr.length-1);
+        }
+    }
+    return "";
+}
